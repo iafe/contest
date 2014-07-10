1afa997ca42ce5b33aef594e35855eb033eb81db
iafe <tech@fairsandexpos.com>
2014-07-10 11:41:07 -0500

    aster

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,25 +13,6 @@ class User < ActiveRecord::Base
   has_many :divisions, through: :judges
   has_many :judges, dependent: :restrict_with_exception
   
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  
-  validates :first_name, presence: true, length: { maximum: 30,  minimum: 2 }
-  validates :last_name, presence: true, length: { maximum: 50,  minimum: 2 }
-  validates :suffix, allow_blank: true, length: { maximum: 10,  minimum: 2 }
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false, 
-    message: "already exists in our database under another user's record. Is this you? Please call the IAFE office." }, length: {maximum: 50 }
-  validates :email_confirmation, presence: true, if: :new_record?
-  validates :email_confirmation, presence: true, if: :email_changed?
-  validates_confirmation_of :email, if: :new_record?
-  validates_confirmation_of :email, if: :email_changed?
-  validates :phone, presence: true, length: { maximum: 20,  minimum: 9 }
-  
-  before_save { self.email = email.downcase }
-  before_save { self.first_name = first_name.titleize }
-  before_save { self.last_name = last_name.titleize }
-  
-  def first_last_email
-    "#{last_name}, #{first_name} - #{email}"
-  end
+
 
 end
