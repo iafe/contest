6fd55b9d09795ee796d665dc51b726b94cb55c05
iafe <tech@fairsandexpos.com>
2014-07-11 10:16:05 -0500

    aster

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ RachelMundhenke::Application.configure do
     :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
     :enable_starttls_auto => true, # detects and uses STARTTLS
     :user_name => ENV['MANDRILL_USERNAME'],
-    :password  => ENV['MANDRILL_PASSWORD'], # SMTP password is any valid API key
+    :password  => ENV['MANDRILL_API_KEY'], # SMTP password is any valid API key
     :authentication => 'login', # Mandrill supports 'plain' or 'login'
     :domain => ENV['MANDRILL_DOMAIN'], # your domain to identify your server when connecting
   }
