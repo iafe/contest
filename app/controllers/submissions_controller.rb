e4dda2f95176018c64d7e3c178aec57f85060116
iafe <tech@fairsandexpos.com>
2014-07-18 15:55:20 -0500

    aster

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -7,7 +7,7 @@ class SubmissionsController < ApplicationController
   # GET /submissions.json
   def index
     @award_deadline = Deadline.order("created_at").last
-    @paginate = current_user.submissions.joins(:organization).includes(:organization).page(params[:page]).per(3).group([:contest_year, :organization_id]).order('contest_year DESC') 
+    @paginate = current_user.submissions.group([:contest_year, :organization_id]).order('contest_year DESC').includes(:organization).page(params[:page]).per(3)
   end
   
   # GET /submissions/1
