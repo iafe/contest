ef9c5093d194fb33b09a9590658703d3fa7a9999
iafe <tech@fairsandexpos.com>
2014-07-22 10:18:36 -0500

    aster

--- app/models/judge.rb
+++ app/models/judge.rb
@@ -4,7 +4,7 @@ class Judge < ActiveRecord::Base
   belongs_to :category
   belongs_to :division
   
-  validates :user_id, presence: true, uniqueness: {scope: :category_id, scope: :division_id, message: "has already been assigned to this 
+  validates :user_id, presence: true, uniqueness: {scope: [:category_id, :division_id], message: "has already been assigned to this 
     category and division." }
   validates :category_id, presence: true
   validates :division_id, presence: true
