da2dd7e7b679769a18d08c77a953779d813c9046
Rachel Dykes <rdykes0715@gmail.com>
2017-02-06 13:48:39 -0600

    Increase award rules character limit

--- app/models/award.rb
+++ app/models/award.rb
@@ -5,7 +5,7 @@ class Award < ActiveRecord::Base
   
   validates :name, presence: true, length: { maximum: 100,  minimum: 1 }
   validates :description, presence: true, length: { maximum: 2000,  minimum: 1 }
-  validates :rules, presence: true, length: { maximum: 10000,  minimum: 1 }
+  validates :rules, presence: true, length: { maximum: 20000,  minimum: 1 }
   validates :sponsor, length: { maximum: 100,  minimum: 1 }, allow_blank: true
   validates :sponsor_logo, length: { maximum: 300,  minimum: 1 }, allow_blank: true
   
